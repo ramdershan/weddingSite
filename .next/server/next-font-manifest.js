2a233d166d802bb924be3c2119dbe31899cb33ac
Raphael Chevallier <raphaelchevallier@hotmail.com>
2025-03-29 11:55:20 -0700

    get rid of .next folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/rchevallier/projects/weddingSite/app/layout\":[\"static/media/fc6b86356f45d8cd-s.p.woff2\",\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
